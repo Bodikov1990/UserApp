dafc68776aef598b2beb659a51b209eed109e2cc
Bodikov Kuat <kuat.bodikov@gmail.com>
2021-12-14 00:58:31 +0500

    Initial Commit

---
+++ UserApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserApp
+//
+//  Created by Kuat Bodikov on 14.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
