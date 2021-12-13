9bac07aa6a5688d448b6c802df17c8f338c5f246
Bodikov Kuat <kuat.bodikov@gmail.com>
2021-12-14 01:56:16 +0500

    created WelcomeViewController and set constraints

---
+++ UserApp/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeViewController.swift
+//  UserApp
+//
+//  Created by Kuat Bodikov on 14.12.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
