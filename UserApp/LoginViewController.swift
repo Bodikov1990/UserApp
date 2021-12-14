9bd54e7f3ca6a5986cbdf81572cae629e4581df8
Bodikov Kuat <kuat.bodikov@gmail.com>
2021-12-14 17:06:34 +0500

    Started logic

--- UserApp/LoginViewController.swift
+++ UserApp/LoginViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var userInputTF: UITextField!
+    @IBOutlet weak var passwordInputTF: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
+    
+    var userInput: String!
+    var passwordInput: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        userInputTF.text = userInput
+        passwordInputTF.text = passwordInput
     }
 
-
+    @IBAction func logInAction() {
+      
+    }
+    
+    
 }
 
--- UserApp/WelcomeViewController.swift
+++ UserApp/WelcomeViewController.swift
@@ -8,11 +8,22 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
+    @IBOutlet weak var userLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let loginViewController = segue.destination as! LoginViewController
+        loginViewController.userInput = userLabel.text
+        
+    }
 
-
+    @IBAction func logOutAction() {
+        dismiss(animated: true)
+    }
+    
 }
